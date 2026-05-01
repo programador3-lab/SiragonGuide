6202dde0c1ea123a9827f8d4f84490f8906f5d02
Programador Ivoo 3 <programador03@SIRAGON.CORP>
2026-05-01 09:34:11 -0400

    lino1

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { writeFile, mkdir } from "fs/promises";
 import { join } from "path";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
